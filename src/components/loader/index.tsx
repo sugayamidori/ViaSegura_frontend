5454a98a4024d39dcfea370808e2ea719d31f54d
Pedro Rodrigues <pedro.rodrigues@compasso.com.br>
2025-11-30 01:01:48 -0300

    fix(ui): allow className overrides in PrimarySpinner component

--- src/components/loader/index.tsx
+++ src/components/loader/index.tsx
@@ -11,7 +11,12 @@ const Loader = ({ className }: React.ComponentProps<"div">) => {
 
 const PrimarySpinner = ({ className }: React.ComponentProps<"div">) => {
   return (
-    <div className="min-h-screen bg-gradient-hero flex items-center justify-center">
+    <div
+      className={cn(
+        "min-h-screen bg-gradient-hero flex items-center justify-center",
+        className
+      )}
+    >
       <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-primary"></div>
     </div>
   );
